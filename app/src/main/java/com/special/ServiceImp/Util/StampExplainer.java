509b58de16c843d95fed97e66868693835ed1edb
arifreyrg <arifreyr@gmail.com>
2015-05-10 15:13:43 +0000

    beinagrind

---
+++ app/src/main/java/com/special/ServiceImp/Util/StampExplainer.java
@@ -0,0 +1,32 @@
+package com.special.ServiceImp.Util;
+
+import com.special.DataStorage.Objects.DataStamp;
+
+/**
+ * Created by Ari on 10.5.2015.
+ */
+public class StampExplainer implements Stamp{
+
+    DataStamp myStamp;
+
+    @Override
+    public String getLongDescription() {
+        
+        return null;
+    }
+
+    @Override
+    public String getShortDescription() {
+        return null;
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
